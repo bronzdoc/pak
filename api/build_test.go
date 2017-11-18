10eb0e187b3e03f2ae99527c3d3d529dd5314194
bronzdoc <lsagastume1990@gmail.com>
2017-11-18 17:49:18 -0600

    Refactor build tests

--- api/build_test.go
+++ api/build_test.go
@@ -1,36 +1,41 @@
 package api_test
 
 import (
+	"fmt"
+
 	"github.com/bronzdoc/pak/api"
 	"github.com/bronzdoc/pak/pakfile"
 
+	"os"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"os"
 )
 
 var _ = Describe("Build", func() {
 	var pakFile *pakfile.PakFile
+	testDir := "/tmp/test_pak"
+	artifactName := "pak-test-artifact"
 
 	BeforeEach(func() {
-		os.Mkdir("/tmp/test_pac", 0777)
+		os.Mkdir(testDir, 0777)
 
 		pakFile = &pakfile.PakFile{}
-		pakFile.ArtifactName = "pak-test-artifact"
-		pakFile.Path = "/tmp/test_pac"
+		pakFile.ArtifactName = artifactName
+		pakFile.Path = testDir
 		pakFile.Metadata = map[string]string{
 			"key": "value",
 		}
 	})
 
 	AfterEach(func() {
-		os.Remove("./tmp/test_pac")
-		os.Remove("pak-test-artifact.tar")
+		os.RemoveAll(testDir)
+		os.Remove(fmt.Sprintf("%s.tar", artifactName))
 	})
 
 	It("should build a pak package", func() {
 		api.Build(pakFile)
-		_, err := os.Stat("pak-test-artifact.tar")
+		_, err := os.Stat(fmt.Sprintf("%s.tar", artifactName))
 		Expect(os.IsNotExist(err)).To(Equal(false))
 	})
 })
